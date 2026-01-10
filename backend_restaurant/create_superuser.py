1b6a488a5614d4be2cf12399dee8751291878208
NaumVlavceski <nvlavceski542@gmail.com>
2026-01-10 21:37:57 +0100

    add superuser

--- backend_restaurant/create_superuser.py
+++ backend_restaurant/create_superuser.py
@@ -4,17 +4,14 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Restorant.settings")
 django.setup()
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created")
+else:
+    print("Superuser already exists")
